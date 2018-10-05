f0966442270da77b8b09ff065498301c120be49d
Masonfrith <gedsaro@gmail.com>
2018-10-04 21:00:03 -0600

    first commit

---
+++ CMPT436_Assignment1_Java_mnf436/src/Main/Main.java
@@ -0,0 +1,14 @@
+package Main;
+
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub1
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ CMPT436_Assignment1_Java_mnf436/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author gedsa
+ *
+ */
+module cmpt436_Assignment1_Java_mnf436 {
+}
