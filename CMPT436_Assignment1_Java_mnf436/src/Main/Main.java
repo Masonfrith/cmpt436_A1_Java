a21a840c11de49dd5155bc79ad8419aa94dc1124
Masonfrith <35181333+Masonfrith@users.noreply.github.com>
2018-10-04 21:07:17 -0600

    test update in comment

--- CMPT436_Assignment1_Java_mnf436/src/Main/Main.java
+++ CMPT436_Assignment1_Java_mnf436/src/Main/Main.java
@@ -3,7 +3,7 @@ package Main;
 public class Main {
 
 	public Main() {
-		// TODO Auto-generated constructor stub1
+		// TODO Auto-generated constructor stub1 online test
 	}
 
 	public static void main(String[] args) {
