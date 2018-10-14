57c36652839ead27a9da18efff9f092d23fa4269
Masonfrith <gedsaro@gmail.com>
2018-10-14 05:07:43 -0600

    Minor text edits, no code change.

--- CMPT436_Assignment1_Java_mnf436/src/Main/Main.java
+++ CMPT436_Assignment1_Java_mnf436/src/Main/Main.java
@@ -2,9 +2,6 @@ package Main;
 
 public class Main {
 
-	public Main() {
-		// TODO Auto-generated constructor stub1 online test
-	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
