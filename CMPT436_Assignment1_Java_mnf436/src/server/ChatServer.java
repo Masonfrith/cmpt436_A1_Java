47b8076a0ea8070127021a52f7d7a9658492cf22
Masonfrith <gedsaro@gmail.com>
2018-10-17 17:34:17 -0600

    add better default ip and port handling for server.
    add possiblity of args to set server port and ip. (ip I think will not
    actually be used, but is here for now anyway).

--- CMPT436_Assignment1_Java_mnf436/src/server/ChatServer.java
+++ CMPT436_Assignment1_Java_mnf436/src/server/ChatServer.java
@@ -9,12 +9,32 @@ import java.net.Socket;
 
 public class ChatServer {
 	
+	private static final int defaultPort = 40008;
+	private static final String defaultIP = "localhost";
+	static int serverPort;
+	static String serverIP;
 	ServerSocket serverSocket;
 	Socket clientSocket;
 	PrintWriter out;
 	BufferedReader in;
 	
 	public static void main(String[] args) {
+		
+		// Basic deal with args, limited, 1 means setting server port, 2 means setting port and ip, anything else uses defaults
+		if (args.length == 1) {
+			serverPort = Integer.parseInt(args[0]);
+			serverIP = defaultIP;
+		} else if(args.length == 2) {
+			serverPort = Integer.parseInt(args[0]);
+			serverIP = args[1];
+		}
+		else {
+			serverPort = defaultPort;
+			serverIP = defaultIP;
+		}
+		
+		
+		
 		
 	}
 	
