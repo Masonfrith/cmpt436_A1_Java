92fa5b6b796080b8938c8d54a3ffab4af8816323
Masonfrith <gedsaro@gmail.com>
2018-10-19 13:48:11 -0600

    move Server creation and start up in to consturctor method...
    add message holding var for server, add program loop which takes a
    message once connected, displays message on server side, and sends a
    message back to client which is either a request to talk more followed
    by a random number, or the first 5 chars of the recived message followed
    by a random number.
    when server recives texxt 'QUIT' the loop ends and the server shuts
    down.

--- CMPT436_Assignment1_Java_mnf436/src/server/ChatServer.java
+++ CMPT436_Assignment1_Java_mnf436/src/server/ChatServer.java
@@ -13,9 +13,12 @@ public class ChatServer {
 	private static final String defaultIP = "localhost";
 	static int serverPort;
 	static String serverIP;
-	ServerSocket serverSocket;
-	static PrintWriter out;
-	static BufferedReader in;
+	
+	ServerSocket serverListener;
+	Socket clientSocket;
+	PrintWriter out;
+	BufferedReader in;
+	String currentMessage;
 	
 	public static void main(String[] args) throws Exception {
 		
@@ -33,27 +36,46 @@ public class ChatServer {
 		}
 		
 		System.out.println("Chat Server starting up, using Port: " + serverPort);
-		ServerSocket serverListener = new ServerSocket(serverPort);
-		Socket clientSocket = new Socket();
+		ChatServer chatServer = new ChatServer(serverPort, serverIP);
+
+		// here should be the loop of whatever the server is going to do.
+		// will do a test, server should keep running until a client sends the text 'QUIT',
+		// when server recives a message, it will display to its console, and send a random number along with the first few
+		// letters of the message it was sent back to the client.
+		while(chatServer.currentMessage != "QUIT"){
+			chatServer.currentMessage = chatServer.in.readLine();
+			System.out.println("Client says: " + chatServer.currentMessage);
+			
+			if(chatServer.currentMessage.length()<=4) {
+				chatServer.out.println("TALK MORE! Here is a number! " + Math.random()*100);
+			}
+			else {
+				chatServer.out.println("" + chatServer.currentMessage.substring(0,5) + "" + Math.random()*1000);
+			}
+			
+		}
+		System.out.println(chatServer.currentMessage + " Recived, shutting down");
 		
-		System.out.println("Server now waiting for a connection.");
-		clientSocket = serverListener.accept();
 		
-		out = new PrintWriter(clientSocket.getOutputStream(), true);
-		in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-		
-		//while (some var != some qutting text) keep waiting for input, then repeat the input with some mod
-		String textTest = in.readLine();
-		System.out.println("Server has recived initial message " + textTest + " from client upon connection");
-		
-		in.close();
-		out.close();
-		clientSocket.close();
-		serverListener.close();
+		System.out.println("Server shutting down.");
+		chatServer.in.close();
+		chatServer.out.close();
+		chatServer.clientSocket.close();
+		chatServer.serverListener.close();
+		System.out.println("Server shutdown completed.");
 	}
 	
-	public ChatServer() {
+	public ChatServer(int portNumber, String ip) throws IOException {
 		
+		this.serverListener = new ServerSocket(portNumber);
+		this.clientSocket = new Socket();
+		this.currentMessage = "";
+		
+		this.clientSocket = serverListener.accept();
+		System.out.println("Server now waiting for a connections.");
+		
+		this.out = new PrintWriter(clientSocket.getOutputStream(), true);
+		this.in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 	}
 	
 	
