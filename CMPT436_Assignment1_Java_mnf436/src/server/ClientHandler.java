2ed2a590005505efcef448d40a8f53d747a15304
Masonfrith <gedsaro@gmail.com>
2018-10-25 18:14:11 -0600

    add new class ClientHandler...
    This class Will implment Runnable from threads.
    It is to be used to handle the servers connections to clients, allowing
    multiple to be serviced at one time.

---
+++ CMPT436_Assignment1_Java_mnf436/src/server/ClientHandler.java
@@ -0,0 +1,15 @@
+package server;
+
+public class ClientHandler implements Runnable {
+
+	public ClientHandler() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
